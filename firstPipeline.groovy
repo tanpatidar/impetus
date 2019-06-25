878e48762225a55a78bb5cb213fa220ce2b1e06e
tanpatidar <51694834+tanpatidar@users.noreply.github.com>
2019-06-25 16:09:55 +0530

    Create firstPipeline.groovy

---
+++ firstPipeline.groovy
@@ -0,0 +1,18 @@
+pipeline {
+    stages {
+        stage('stage 1') {
+            steps {
+                script{
+                    //do your things of first pipeline stage
+                }
+            }
+        }
+        stage('stage 2') {
+            steps {
+                script{
+                    //do your things of first pipeline stage
+                }
+            }
+        }
+    }
+}
